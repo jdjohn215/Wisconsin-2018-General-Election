9e19646a719449618e7818454827259d82d88bdc
John D. Johnson <jdjohn215@gmail.com>
2020-06-25 14:42:50 -0500

    init

---
+++ scripts/CombineStatewideResultsWithAssemblyRaces.R
@@ -0,0 +1,18 @@
+rm(list = ls())
+
+library(tidyverse)
+
+# statewide races in assembly districts
+statewide <- read_csv("data/Wisconsin2018_StatewideRaces_AssemblyDistricts.csv") %>%
+  mutate(assembly_district = word(assembly_district, -1))
+
+# assembly race results
+assembly <- read_csv("data/Wisconsin2018_AssemblyRaces_Summary.csv") %>%
+  # match statewide format
+  rename(assembly_district = district, wsa_dem = Democrat, 
+         wsa_rep = Republican, wsa_total = total) %>%
+  mutate(assembly_district = word(assembly_district, -1))
+
+combine <- inner_join(statewide, assembly)
+
+write_csv(combine, "data/Wisconsin2018_AssemblyDistricts_StatewideRacesAndAssemblyRaces.csv")
